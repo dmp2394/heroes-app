41efcf9dc94981ba1b2cf7c01e882d8573544eb6
David <dmpuerto23@gmail.com>
2025-12-16 15:32:50 -0300

    agrego contexto de favoritos

---
+++ src/heroes/context/FavoriteHeroContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, useEffect, useState, type PropsWithChildren } from 'react'
+import type { Hero } from '../types/hero.interface';
+
+interface FavoriteHeroContext {
+    // State
+    favorites: Hero[];
+    favoriteCount: number;
+
+    // Methods
+    isFavorite: (hero: Hero) => boolean;
+    toggleFavorite: (hero: Hero) => void;
+}
+
+
+export const FavoriteHeroContext = createContext({} as FavoriteHeroContext);
+
+const getFavoritesFromLocalStorage = (): Hero[] => {
+    const favorites = localStorage.getItem('favorites');
+    return favorites ? JSON.parse('favorites') : [];
+}
+
+
+export const FavoriteHeroProvider = ({ children }: PropsWithChildren) => {
+
+    const [favorites, setFavorites] = useState<Hero[]>(getFavoritesFromLocalStorage());
+
+    const isFavorite = (hero: Hero) => {
+        return (favorites.some(h => h.id === hero.id));
+    }
+
+    const toggleFavorite = (hero: Hero) => {
+
+        if (isFavorite(hero)) {
+            setFavorites(favorites.filter(h => h.id !== hero.id));
+            return;
+        };
+
+        setFavorites([...favorites, hero]);
+    };
+
+    useEffect(() => {
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }, [favorites]);
+
+
+    return (
+        <FavoriteHeroContext
+            value={{
+                favoriteCount: favorites.length,
+                favorites: favorites,
+                isFavorite: isFavorite,
+                toggleFavorite: toggleFavorite,
+            }}
+        >
+            {children}
+
+        </FavoriteHeroContext>
+    )
+}
